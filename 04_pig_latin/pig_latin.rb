bd600d21c63d518bd82ecaf192e2e6026f240ced
Steven Havko <stevenhavko@gmail.com>
2015-01-06 12:38:32 -0500

    Final Commit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,4 +1,21 @@
-def translate(word)
-  prefix = word[0, %w(a e i o u).map{|vowel| "#{word}aeiou".index(vowel)}.min]
-  "#{word[prefix.length..-1]}#{prefix}ay"
+# author: Alex Chaffee
+
+def translate phrase
+  phrase.split.map do |word|
+
+    # we are using "/x" to document this big fat regex
+    word =~ /^     # beginning of string
+    (
+      [^aeiouyq]*  # any number of consonants in a row
+      (qu)?        # or maybe a 'qu'
+    )
+    (.*)           # the rest of the string
+    $/x            # end of string
+
+    # $1, $2, etc. get filled with the parenthesized chunks
+    # from the most recent regular expression match
+    first_phoneme = $1
+    rest_of_word = $3
+    "#{rest_of_word}#{first_phoneme}ay"
+  end.join(" ")
 end
